6f8a9f0ddddb3a87c65918362128da228d89b3e6
FuturePhile <hradziewicz1@gmail.com>
2024-06-27 10:30:07 +0200

    123

--- led_driver.v
+++ led_driver.v
@@ -7,27 +7,52 @@ module led_driver (
 
 //parametry dla 50MHz
 
-parameter T0H = 20;
+parameter T0H = 21;
 parameter T0L = 43;
 parameter T1H = 41;
 parameter T1L = 23;
+parameter TP = 64;
 parameter TR = 2560;
 
 reg [4:0] bit_counter;
 reg [23:0] color_data;
+reg [11:0] counter;
+wire data;
 
 
 always @(posedge CLK or posedge RST) begin
     if (RST) begin
         bit_counter <= 5'b00000;
+        counter <= 11'd0;
         color_data <= color;
     end
     else if (CLK) begin
         if (bit_counter < 5'b1100) begin
             if (color_data[bit_counter] == 1'b1) begin
-
+                if (counter < T1H) begin
+                    data <= 1'b1;
+                    counter <= counter + 1;
+                end else if (counter < T1L) begin
+                    data <= 1'b0;
+                    counter <= counter + 1;
+                end else begin
+                    counter <= 11'b0;
+                end
             end else if (color_data[bit_counter] == 1'b0) begin
-
+                if (coutnter < T0H) begin
+                    data <= 1'b1;
+                    counter <= counter + 1;
+                end else if (counter < T0L) begin
+                    data <= 1'b0;
+                    counter <= counter + 1;
+                end else begin
+                    counter <= 11'b0;
+                end
+            end else if (bit_counter == 5'b11000) begin
+                if (counter < TR) begin
+                    data <= 1'b1;
+                    counter <= counter + 1;
+                end
             end else begin
                 bit_counter <= bit_counter + 1;
             end
@@ -38,5 +63,7 @@ always @(posedge CLK or posedge RST) begin
 
     end
 end
+
+assign LED <= data;
 
 endmodule
